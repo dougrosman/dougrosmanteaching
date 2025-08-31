a13dbd0ada1c0f1486c371182479591de3018137
Doug Rosman <dougrosman@gmail.com>
2025-08-30 19:47:24 -0500

    Quartz sync: Aug 30, 2025, 7:47 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Inter",
-        body: "Source Sans Pro",
+        header: "Inconsolata",
+        body: "Inconsolata",
         code: "IBM Plex Mono",
       },
       colors: {
@@ -68,7 +68,7 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
+      //Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
